5a151642aca741cda35ea9ddc4c3471b97a32c5f
bhavatharanim <tharanitharu2201@gmail.com>
2021-04-29 17:58:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {Login} from './components/login';
+import Vote from './components/user/vote';
+import {Signup} from './components/signup';
+import Poll1 from './components/user/pol1';
+import Success from './components/user/success';
+import {LoginAdmin} from './components/admin/login';
+import List from './components/admin/list';
+import {Candidate} from './components/admin/addcandidate';
+import Error from "./error";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+    <Switch>
+      <Route path="/" exact component={Login} />
+      <Route path="/vote" exact component= {Vote} />
+      <Route path="/signup" exact component= {Signup} />
+      <Route path="/poll1" exact component = {Poll1} />
+      <Route path="/success" exact component = {Success} />
+      <Route path="/admin" exact component = {LoginAdmin} />
+      <Route path="/list" exact component = {List} />
+      <Route path="/addcan" exact component = {Candidate} />
+      <Route path="*" component={Error} />
+    </Switch>
+  </Router>
+  );
+}
+
+export default App;
